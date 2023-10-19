9bfe4fd5fad850427e893bcd83dc34bbcd28a529
KazukiAtwood <103943752+KazukiAtwood@users.noreply.github.com>
2023-10-19 16:43:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,281 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href='style.css' rel='stylesheet'>
+  </head>
+  <body>
+
+  <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="https://www.codeur.com/tuto/wp-content/uploads/2021/12/logo.png" alt="" class="d-inline-block align-text-top">
+        MatchMentor
+    </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Devenir mentor</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Trouver un mentor</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inscrire un jeune</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Entreprises</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Connectez-vous</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inscrivez-vous</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+        
+        <div class='mentorat'>
+        <h1><img src="img.png" width="582" height="330" align="right">Le mentorat c'est quoi ?</h1>
+
+        <p>Le mentorat, c’est l’accompagnement individuel d’un jeune de 5 à 30 ans par une personne bénévole pour l’aider au mieux se connaître, s’épanouir, prendre confiance en lui et développer son autonomie.</p>
+        <p>"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."<br clear="all"><p/>
+        </div>
+        
+        <h1 class='binome'><center>Le binôme de confiance</center></h1>
+        <div align="center" class='promo'>
+        <table border="0" cellpadding="4" width="1300">
+         <tr>
+             <th><h3><center>Un engagement personnel</center></h3></th>
+          <th><h3><center>Avec la durée</center></h3></th>
+          <th><h3><center>Avec un suivi</center></h3></th>
+         </tr>
+         <tr>
+          <td align="center"><img src="cercle.jpg" width="200" height="200"></td>
+          <td align="center"><img src="cercle.jpg" width="200" height="200"></td>
+          <td align="center"><img src="cercle.jpg" width="200" height="200"></td>
+         </tr><tr>
+          <td align="center" valign="top">"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."</td>
+          <td align="center" valign="top">"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."</td>
+          <td align="center" valign="top">"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."</td>
+         </tr>
+        </table>
+        </div>
+        
+        <div align="center" class='trouve'>
+        <table border="0" cellpadding="4" width="1300">
+         <tr>
+          <th><button type="button" class="btn btn-secondary">Un engagement personnel</button></th>
+          <th><button type="button" class="btn btn-secondary">Dans la durée</button></th>
+          <th><button type="button" class="btn btn-secondary">Avec un suivi</button></th>
+         </tr>
+        </table>
+        </div>
+        
+        <div class="question">
+            <div class="text-left">
+                <h3>Qui peut être mentoré ?</h3>
+                <button type="button" class="btn btn-light">Ecolier</button>
+                <button type="button" class="btn btn-light">Collegien</button>
+                <button type="button" class="btn btn-light">Etudiant</button>
+                <button type="button" class="btn btn-light">Lycée</button>
+                <button type="button" class="btn btn-light">Apprenti</button>
+            </div>
+            <div class="text-right">
+                <h3>Qui peut être mentor ?</h3>
+                <button type="button" class="btn btn-light">Salarié</button>
+                <button type="button" class="btn btn-light">Etudiant</button>
+                <button type="button" class="btn btn-light">Retraité</button>
+                <button type="button" class="btn btn-light">Lycéen</button>
+                <button type="button" class="btn btn-light">Agent public</button>
+            </div>
+        </div>
+        
+        <div align="center" class='argument'>
+        <table border="0" cellpadding="4" width="1300">
+         <tr>
+          <td align="center"><img src="img.png" width="410" height="300" alt="un engagement personnel"></td>
+          <td align="center"><img src="img.png" width="410" height="300" alt="dans la durée"></td>
+          <td align="center"><img src="img.png" width="410" height="300" alt="avec un suivi"></td>
+         </tr><tr>
+          <th><h5>Plus facile, faite vous embaucher pour vos talents</h5></th>
+          <th><h5>Accélérer l'obtention de connaissances</h5></th>
+          <th><h5>Augmenter le développement avec de merveilleux mentor</h5></th>
+         </tr><tr>
+             <td valign="top">"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."<br><br><button type="button" class="btn btn-secondary btn-lg">Enrengistrez-vous</button></td>
+             <td valign="top">"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."<br><br><button type="button" class="btn btn-secondary btn-lg">Obtenez un match</button></td>
+             <td valign="top">"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."<br><br><button type="button" class="btn btn-secondary btn-lg">Découvrez nos mentors</button></td>
+         </tr>
+        </table>
+        </div>        
+        <div class='mentor'>
+            <button type="button" class="btn btn-secondary">Découvrez vos futurs mentors</button>
+            <img src="flecheg.png">
+            <img src="fleched.png">
+        </div>    
+       
+        <div align="center" class='découverte'>
+        <table border="0" cellpadding="4" width="1200">
+         <tr>
+          <td align="center"><img src="img.png" width="250" height="250" alt="un engagement personnel"></td>
+          <td align="center"><img src="img.png" width="250" height="250" alt="dans la durée"></td>
+          <td align="center"><img src="img.png" width="250" height="250" alt="avec un suivi"></td>
+          <td align="center"><img src="img.png" width="250" height="250" alt="avec un suivi"></td>
+         </tr><tr>
+          <th><small>Disponible | Expertise : Design</small></th>
+          <th><small>Disponible | Expertise : Design</small></th>
+          <th><small>Disponible | Expertise : Design</small></th>
+          <th><small>Disponible | Expertise : Design</small></th>
+         </tr>
+        </table>
+        </div>
+        
+        <br><br><br>
+        <h3><center>Voyez ce que nos membres vous disent</center></h3>
+        <br><br><br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text">"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."</p>
+                                <p class="card-text">Jean-Jacques Martyr</p>
+                                <p class="card-text">Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text">"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."</p>
+                                <p class="card-text">Michel Branche</p>
+                                <p class="card-text">Développeur web</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <p class="card-text">"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."</p>
+                                <p class="card-text">Lin Feng</p>
+                                <p class="card-text">Cultivateur</p>
+                            </div>
+                        </div>
+                    </div>
+            </div>        
+        <div class='cause'>
+        <div>
+        <h1><img src="img.png" width="600" height="400" align="right" hspace="20">Le mentorat c'est une cause nationale</h1>
+
+        <p>"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."</p>
+        <p>"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."</p>
+        <p>"Il n’y a personne qui n’aime la souffrance pour elle-même, qui ne la recherche et qui ne la veuille pour elle-même..."
+        <br><br><button type="button" class="btn btn-secondary">En savoir plus</button><br clear="all"><p/>
+        </div>
+        </div>
+                
+                <br><br>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <h2>Rejoignez-nous</h2>
+                </div>
+                <div class="col-md-6">
+                    <form>
+                        <div class="form-group">
+                            <label for="prenom">Prénom</label>
+                            <input type="text" id="prenom" name="prenom" class="form-control" placeholder='Patrick'>
+                        </div>
+                        <div class="form-group">
+                            <label for="nom">Nom</label>
+                            <input type="text" id="nom" name="nom" class="form-control" placeholder="NOYON">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">E-mail</label>
+                            <input type="email" id="email" name="email" class="form-control" placeholder='patricknoyon504@gmail.com'>
+                        </div>
+                        <div class="form-group">
+                            <label for="expertises">Expertises souhaitées</label>
+                            <select id="expertises" name="expertises" class="form-control">
+                                <option value="web">Web</option>
+                                <option value="mobile">Mobile</option>
+                                <option value="data">Data Science</option>
+                                <option value="design">Design</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="ville">Ville</label>
+                            <input type="text" id="ville" name="ville" class="form-control" placeholder='Mantes-la-Jolie'>
+                        </div><br>
+                        <button type="submit" class="btn btn-primary">Inscrivez-vous</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div align="center" class='trouve'>
+        <table border="0" cellpadding="4" width="1300">
+         <tr>
+          <th><button type="button" class="btn btn-secondary">Je trouve un mentor</button></th>
+          <th><button type="button" class="btn btn-secondary">J'inscris un jeune de mon âge</button></th>
+          <th><button type="button" class="btn btn-secondary">Je deviens mentor</button></th>
+         </tr>
+        </table>
+        </div>
+       
+                <br><br><br>
+        <!-- Footer -->
+<footer class="bg-light text-center ">
+  <!-- Grid container -->
+  <div class="container p-4">
+
+
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <img src="https://www.codeur.com/tuto/wp-content/uploads/2021/12/logo.png" alt="" class="d-inline-block align-text-top">
+        MatchMentor
+        <button type="button" class="btn btn-light">Inscrivez vous à notre newsletter</button>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <a href="#!" role="button"><img src="facebook.jpg" alt="Description de l'image"></a>
+              <a href="#!" role="button"><img src="twitter.jpg" alt="Description de l'image"></a>
+              <a href="#!" role="button"><img src="youtube.jpg" alt="Description de l'image"></a>
+              <br>
+              Copyright 2023 - <strong>Matchmentor</strong>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+              <p href="#!" class="text-dark">Devenir un mentor</p>
+              <p href="#!" class="text-dark">Trouver un mentor</p>
+              <p>Mentions légales</p>
+              <p>Politiques de confidentialité</p>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+
+  </div>
+
+</footer>
+  </body>
+</html>
---
+++ test.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Inclure la bibliothèque Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Carte 1</h5>
+                        <p class="card-text">Contenu de la carte 1.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Carte 2</h5>
+                        <p class="card-text">Contenu de la carte 2.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Carte 2</h5>
+                        <p class="card-text">Contenu de la carte 2.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Inclure la bibliothèque Bootstrap JS (si nécessaire) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+</html>
